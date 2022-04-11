bbfe6430a21637e36fa8ff45d33d759098090e29
Sean McQuaid <sean@decrypteddesign.com>
2022-04-11 15:39:35 -0700

    feat | add esm module to /dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,33 @@
 import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 
-export default {
-  input: 'src/index.js',
-  plugins: [
-    terser({
-      //   mangle: {
-      //     properties: {
-      //       regex: /els/, //mangles this.els
-      //     },
-      //   },
-    }),
-    filesize(),
-  ],
-  output: {
-    sourcemap: true, //can pass 'inline' for inline source maps
-    file: 'dist/quantdom.min.js',
-    format: 'umd',
-    name: '$',
+export default [
+  {
+    input: 'src/index.js',
+    plugins: [
+      terser({
+        //   mangle: {
+        //     properties: {
+        //       regex: /els/, //mangles this.els
+        //     },
+        //   },
+      }),
+      filesize(),
+    ],
+    output: {
+      sourcemap: true, //can pass 'inline' for inline source maps
+      file: 'dist/quantdom.min.js',
+      format: 'umd',
+      name: '$',
+    },
   },
-};
+  {
+    input: 'src/index.js',
+    plugins: [terser(), filesize()],
+    output: {
+      sourcemap: true, //can pass 'inline' for inline source maps
+      file: 'dist/quantdom.esm.min.js',
+      format: 'es',
+    },
+  },
+];
